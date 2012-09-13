321fe80f296171388eca7f3b68a83036f9988bab
Artur Dorochowicz <artur@dorochowicz.com>
2012-09-13 20:37:06 +0200

    Added knockout and structure.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,13 @@
-define([], function() {
-  return 'aaaaaa Hello from Yeoman!';
+define(['jquery', 'ko'], function ($, ko) {
+	"use strict";
+
+	function MyViewModel() {
+		this.name = ko.observable('artur');
+	}
+
+	$.extend(MyViewModel.prototype, {
+	});
+
+	return new MyViewModel();
 });
+
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,11 +3,20 @@ require.config({
   },
 
   paths: {
-    jquery: 'vendor/jquery.min'
+    jquery: 'vendor/jquery.min',
+	ko: 'vendor/knockout-2.1.0.debug'
   }
 });
- 
-require(['app'], function(app) {
-  // use app here
-  console.log(app);
+
+require(['ko', 'jquery', 'app'], function (ko, $, app) {
+	var module, container;
+
+	module = $(document.body).data('module');
+	container = $(document.body).data('container');
+
+	require([module || 'app'], function (vm) {
+		$(function () {
+			ko.applyBindings(vm, $(container ? '#' + container : '#container').show().get(0));
+		});
+	});
 });
