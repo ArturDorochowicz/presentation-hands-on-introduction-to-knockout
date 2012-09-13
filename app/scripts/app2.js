02bf9b49c5c51eb7faceae3b9cfc364fcd119d8e
Artur Dorochowicz <artur@dorochowicz.com>
2012-09-13 23:03:59 +0200

    Show binding.

--- app/scripts/app2.js
+++ app/scripts/app2.js
@@ -5,9 +5,29 @@ define(['jquery', 'ko'], function ($, ko) {
 		this.r = ko.observable(0);
 		this.g = ko.observable(0);
 		this.b = ko.observable(0);
+
+		//this.addBinding();
 	}
 
 	$.extend(MyViewModel.prototype, {
+		addBinding: function () {
+			ko.bindingHandlers.canvasColor = {
+				init: function (element, valueAccessor, allBindingsAccessor, context) {
+					var ctx = element.getContext('2d'),
+						values = ko.utils.unwrapObservable(valueAccessor());
+
+					ctx.fillStyle = 'rgb(' + values[0] + ', ' + values[1] + ', ' + values[2] + ')';
+					ctx.fillRect(0, 0, 200, 200);
+				},
+				update: function (element, valueAccessor, allBindingsAccessor, context) {
+					var ctx = element.getContext('2d'),
+						values = ko.utils.unwrapObservable(valueAccessor());
+
+					ctx.fillStyle = 'rgb(' + values[0] + ', ' + values[1] + ', ' + values[2] + ')';
+					ctx.fillRect(25, 25, 150, 150);
+				}
+			};
+		}
 	});
 
 	return new MyViewModel();
