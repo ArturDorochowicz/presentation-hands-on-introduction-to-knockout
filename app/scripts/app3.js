445958b1d6860df56489dd6b1ef6af608f96a336
Artur Dorochowicz <artur@dorochowicz.com>
2012-09-15 21:59:48 +0200

    Add a
    more complete example.

---
+++ app/scripts/app3.js
@@ -0,0 +1,58 @@
+define(['jquery', 'ko'], function ($, ko) {
+	"use strict";
+
+	function Color() {
+		this.r = ko.observable(0);
+		this.g = ko.observable(0);
+		this.b = ko.observable(0);
+
+		this.value = ko.computed(function () {
+			return 'rgb(' + this.r() + ', ' + this.g() + ', ' + this.b() + ')';
+		}, this);
+	}
+
+	function MyViewModel() {
+		this.gradientStops = ko.observableArray();
+		this.gradientStops.push(new Color());
+
+		this.canAddGradientStop = ko.computed(function () {
+			return this.gradientStops().length < 10;
+		}, this);
+	}
+
+	$.extend(MyViewModel.prototype, {
+		addGradientStop: function () {
+			var stop;
+			if (!this.canAddGradientStop()) {
+				return;
+			}
+			stop = new Color();
+			this.gradientStops.push(stop);
+		},
+
+		removeGradientStop: function (gradientStop) {
+			if (this.gradientStops().length <= 1) {
+				return;
+			}
+			this.gradientStops.remove(gradientStop);
+		}
+	});
+
+	ko.bindingHandlers.cg = {
+		update: function (element, valueAccessor) {
+			var gradientStops = ko.utils.unwrapObservable(valueAccessor()),
+				ctx = element.getContext('2d'),
+				gradient = ctx.createLinearGradient(0, 0, 50 * gradientStops.length, 0);
+
+			$.each(gradientStops, function (i) {
+				gradient.addColorStop((i+1)/gradientStops.length, this.value());
+			});
+
+			ctx.fillStyle = gradient;
+			ctx.fillRect(0, 0, 50 * gradientStops.length, 50);
+		}
+	};
+
+	return new MyViewModel();
+});
+
